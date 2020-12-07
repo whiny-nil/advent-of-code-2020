48b61878782a7545795fc2f3d15b8eaf76227772
Marc Kelsey <marc@weeverapps.com>
2020-12-07 08:33:23 -0500

    07b

--- 07/solution.rb
+++ 07/solution.rb
@@ -3,6 +3,7 @@ file_name = File.dirname(__FILE__) + '/input.txt'
 @lines = File.read(file_name).strip.split("\n")
 
 test_input = <<~TESTINPUT
+  light red bags contain 1 bright white bag, 2 muted yellow bags.
   dark orange bags contain 3 bright white bags, 4 muted yellow bags.
   bright white bags contain 1 shiny gold bag.
   muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
@@ -56,7 +57,9 @@ end
 def parse_rules1(lines)
   @rules1 = {}
   
-  lines.map { |line| parse1(line) }
+  lines.each { |line| parse1(line) }
+
+  @rules1
 end
 
 def trace1(bag)
@@ -65,16 +68,55 @@ def trace1(bag)
   rules1[bag] + rules1[bag].map { |b| trace1(b) }
 end
 
-def solution1
+#########
+
+def rules2
+  @rules2
+end
+
+def parse2(line)
+  clean_line = line.gsub("bags", "bag").gsub(".", "")
+  parts = clean_line.split(" contain ")
+  container = parts[0]
+  contents = parts[1]
+  @rules2[container] = []
+
+  parts[1].split(", ").each do |content|
+    next if content == "no other bag"
+    contents = content.split(" ", 2)
+    contents[0].to_i.times do
+      @rules2[container] << contents[1]
+    end
+  end
+end
+
+def parse_rules2(lines)
+  @rules2 = {}
+
+  lines.each { |line| parse2(line) }
+
+  @rules2
+end
+
+def trace2(bag)
+  return 0 if @rules2[bag].nil?
+  return 0 if @rules2[bag].length == 0
+
+  @rules2[bag].length + @rules2[bag].map { |b| trace2(b) }.compact.reduce(&:+)
+end
+
+def solution1(lines)
   parse_rules1(lines)
   trace1("shiny gold bag").flatten.compact.uniq.length
 end
 
-def solution2
-  return "Not implemented!"
+def solution2(lines)
+  parse_rules2(lines)
+  trace2("shiny gold bag")
 end
 
-# assert(solution1, 355)
+# assert(solution1(test_lines), 4)
+# assert(solution2(test_lines), 32)
 
-puts "Solution 1: #{solution1}"
-puts "Solution 2: #{solution2}"
+puts "Solution 1: #{solution1(lines)}"
+puts "Solution 2: #{solution2(lines)}"
