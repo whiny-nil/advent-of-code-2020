4c918446ff21b9ded9e07bb52de9646139d20288
Marc Kelsey <marc.kelsey@gmail.com>
2020-12-07 07:30:25 -0500

    07a

--- 07/solution.rb
+++ 07/solution.rb
@@ -32,14 +32,49 @@ def test_lines
 end
 
 # stuff for this problem
+def rules1
+  @rules1
+end
+
+def parse1(line)
+  clean_line = line.gsub("bags", "bag").gsub(".", "")
+  parts = clean_line.split(" contain ")
+  container = parts[0]
+  contents = parts[1]
+  parts[1].split(", ").each do |content|
+    next if content == "no other bag"
+    normalized_content = content.sub(/[0-9]+ /, "")
+    
+    if @rules1[normalized_content]
+      @rules1[normalized_content] << container
+    else
+      @rules1[normalized_content] = [container]
+    end
+  end 
+end
+
+def parse_rules1(lines)
+  @rules1 = {}
+  
+  lines.map { |line| parse1(line) }
+end
+
+def trace1(bag)
+  return if rules1[bag].nil?
+
+  rules1[bag] + rules1[bag].map { |b| trace1(b) }
+end
 
 def solution1
-  return "Not implemented!"
+  parse_rules1(lines)
+  trace1("shiny gold bag").flatten.compact.uniq.length
 end
 
 def solution2
   return "Not implemented!"
 end
+
+# assert(solution1, 355)
 
 puts "Solution 1: #{solution1}"
 puts "Solution 2: #{solution2}"
