a3b0c32f261fcb96e4f1cde8e08483a1235b7437
Marc Kelsey <marc.kelsey@gmail.com>
2020-12-16 05:53:48 -0500

    16a

---
+++ 16/solution.rb
@@ -0,0 +1,71 @@
+require 'byebug'
+
+# Parse the damn input
+file_name = File.dirname(__FILE__) + '/input.txt'
+@input = File.read(file_name).strip.split("\n\n") if File.exist?(file_name)
+
+test_input = <<~TESTINPUT
+  class: 1-3 or 5-7
+  row: 6-11 or 33-44
+  seat: 13-40 or 45-50
+  
+  your ticket:
+  7,1,14
+  
+  nearby tickets:
+  7,3,47
+  40,4,50
+  55,2,20
+  38,6,12
+TESTINPUT
+@test_input = test_input.strip.split("\n\n")
+
+# helper methods
+def assert(value, expected_value)
+  puts (value == expected_value ? "." : "Failed: Expected #{expected_value.inspect}, got #{value.inspect}")
+end
+
+def parse_ranges(notes)
+  values = []
+  
+  notes.split("\n").each do |note|
+    parts = note.split(': ')
+    ranges = parts[1].split(' or ')
+    r0 = ranges[0].split('-').map { |el| el.to_i }
+    r1 = ranges[1].split('-').map { |el| el.to_i }
+    values = values | (r0[0]..r0[1]).to_a | (r1[0]..r1[1]).to_a
+  end
+
+  values
+end
+
+def parse_tickets(raw_tickets)
+  raw_tickets = raw_tickets.split("\n")
+  raw_tickets.shift
+  raw_tickets.map{ |el| el.split(',').map { |el| el.to_i } }
+end
+
+# stuff for this problem
+def solution1(input)
+  valid_numbers = parse_ranges(input[0])
+  tickets = parse_tickets(input[2])
+  error = 0
+
+  tickets.each do |ticket|
+    ticket.each do |field|
+      error += field unless valid_numbers.include? field
+    end
+  end
+
+  error
+end
+
+def solution2(input)
+  return "Not implemented!"
+end
+
+# assert(solution1(@test_input), 71)
+# assert(solution2(@test_input), nil)
+
+puts "Solution 1: #{solution1(@input)}"
+# puts "Solution 2: #{solution2(@input)}"
